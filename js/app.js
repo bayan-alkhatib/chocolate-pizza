2edce8f86155b2a637e06d48b171e8e6fd16af06
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-04-07 20:26:46 +0300

    complete lab-10 stretch goal (Make the checkboxes actually check using JS)

---
+++ js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+let input=document.getElementsByTagName('input');
+console.log(input);
+let label=document.getElementsByTagName('label');
+console.log(label);
+
+// input.addEventListener('checked',checkbox);
+
+function check () {
+  for(let i=0;i<input.length;i++){
+    if(input[i].type==='checkbox'){
+      input[i].addEventListener('change',checkbox);
+    }
+  }
+}
+
+function checkbox(event){
+  if(event.target.checked===true){
+    for (let j=0;j<label.length;j++){
+      if(event.target.id===label[j].id){
+        label[j].style.textDecoration='line-through' ;
+      }
+    }
+  }else{for (let j=0;j<label.length;j++){
+    if(event.target.id===label[j].id){
+      label[j].style.textDecoration='none';
+    }
+  }
+  }
+}
+
+check();
